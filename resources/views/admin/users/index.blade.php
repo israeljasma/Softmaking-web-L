428c6a0fef53537374dafa2596aca296d28aca2e
Israel Jasma <by_darklancer@live.cl>
2020-09-01 15:57:33 -0400

    Feat: mejoras vista user en admin

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,9 +8,29 @@
                 <div class="card-header">Users</div>
 
                 <div class="card-body">
-                    @foreach ($users as $user)
-                        {{ $user->name }} - {{ $user->email }}
-                    @endforeach
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Acciones</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <th scope="row">{{ $user->id }}</th>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.users.edit', $user->id) }}"><button type="button" class="btn btn-primary">Editar</button></a>
+                                        <a href="{{ route('admin.users.destroy', $user->id) }}"><button type="button" class="btn btn-danger">Eliminar</button></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
