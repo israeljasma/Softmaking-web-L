56d9d5c1ec6d88630a70a7d1b320c7aafc4db516
Israel Jasma <by_darklancer@live.cl>
2020-11-17 22:39:18 -0300

    Style: Codigo muerto eliminado

--- resources/views/user/invoices/index.blade.php
+++ resources/views/user/invoices/index.blade.php
@@ -29,26 +29,6 @@
                     <td class="border-grey-light border hover:bg-gray-100 p-3 h-12 text-left md:text-center"><a class="px-3 md:py-1 mr-2 border border-transparent text-base rounded text-white bg-gray-400 hover:bg-gray-500 focus:outline-none focus:border-gray-500 focus:shadow-outline-blue transition duration-150 ease-in-out md:text-lg"
                             href="{{ route('user.invoices.show', $invoice->id) }}">Ver</a></td>
                     <td class="border-grey-light border hover:bg-gray-100 p-3 h-12 text-left md:text-center"><a href="#" class="px-3 py-1 border border-transparent text-base rounded text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:border-blue-500 focus:shadow-outline-blue transition duration-150 ease-in-out md:text-lg">Descargar</a></td>
-                    {{-- <td>{{ implode(
-                            ',',
-                            $user->roles()->get()->pluck('name')->toArray(),
-                        ) }}</td>
-                    <td><a href="{{ route('admin.invoices.index', $user) }}"><button type="button"
-                                class="btn btn-primary float-left">Ver</button></a></td>
-                    <td>
-                        @can('edit-users')
-                        <a href="{{ route('admin.users.edit', $user->id) }}"><button type="button"
-                                class="btn btn-primary float-left">Editar</button></a>
-                        @endcan
-
-                        @can('delete-users')
-                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST" class="float-left">
-                            @csrf
-                            {{ method_field('DELETE') }}
-                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                        </form>
-                        @endcan
-                    </td> --}}
                 </tr>
             @endforeach
         </tbody>
