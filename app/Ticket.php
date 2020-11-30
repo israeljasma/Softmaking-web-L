f30fd8c347f589510dc6f7f14b30ef79d1e4a79c
Israel Jasma <by_darklancer@live.cl>
2020-11-30 04:22:02 -0300

    Feat: tabla y migracion ticket

---
+++ app/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'user_id', 'category_id', 'ticket_id', 'title', 'priority', 'message', 'status'
+    ];
+}
