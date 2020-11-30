a9c31e90d769f950bc91406dba346459e439be9c
Israel Jasma <by_darklancer@live.cl>
2020-11-30 04:41:01 -0300

    Feat: relaciones

--- app/Ticket.php
+++ app/Ticket.php
@@ -8,8 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'user_id', 'category_id', 'ticket_id', 'title', 'priority', 'message', 'status'
     ];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
