60b56ee0b7613f9d56cf959d34a151e2128007b6
Israel Jasma <by_darklancer@live.cl>
2020-12-02 14:45:48 -0300

    Feat: Tickets button in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,9 @@
         </div>
         <a href="{{ route('user.invoices.index') }}"><button type="submit"
             class="px-8 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition duration-150 ease-in-out md:text-lg md:px-10">Facturas</button></a>
+
+        <a href="{{ route('ticket.index') }}"><button type="submit"
+            class="px-8 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition duration-150 ease-in-out md:text-lg md:px-10">Tickets</button></a>
     </div>
 
 @endsection
