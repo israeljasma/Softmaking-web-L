7562f6fa27a11441474768eb373bd0feb8533b2c
Israel Jasma <by_darklancer@live.cl>
2020-12-09 14:37:33 -0300

    Revert "Feat: mensaje sin tickets"
    This reverts commit 5cecf3572ddf6da88762fa924f9374f10edddb29.

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -4,10 +4,13 @@
     <h2 class="text-3xl text-blue-700 font-bold mb-3">Tickets</h2>
     <a href="{{ route('ticket.create') }}"><button type="submit"
         class="px-8 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition duration-150 ease-in-out md:text-lg md:px-10">Crear</button></a>
-        
-    @if(empty($tickets))
+    
     <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-sm my-5">
+        {{-- @if($tickets->isEmpty())
+            <p>No tiene tickets creados</p>
+        @else --}}
         <thead class="text-white">
+            {{-- @foreach ($tickets as $ticket) --}}
                 <tr class="bg-blue-700 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
                     <th class="p-3 text-left h-12" scope="col">Ticket</th>
                     <th class="p-3 text-left h-12" scope="col">Categoria</th>
@@ -16,6 +19,7 @@
                     <th class="p-3 text-left h-12" scope="col">Última actualización</th>
                     <th class="p-3 text-left h-12" scope="col">Ver</th>
                 </tr>
+            {{-- @endforeach --}}
         </thead>
         <tbody class="flex-1 sm:flex-none">
             @foreach ($tickets as $ticket)
@@ -30,8 +34,5 @@
             @endforeach
         </tbody>
     </table>
-    @else
-        <p>No tiene tickets creados</p>
-    @endif
     
 @endsection
