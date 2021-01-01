091292dce4f8242d36cd8a0fe4c5696d543c8831
Israel Jasma <by_darklancer@live.cl>
2021-01-01 11:25:44 -0300

    Feat: route clients

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('clients', 'Admin\ClientsController', ['except' => []]);
+Route::resource('clients', 'Admin\ClientsController', ['except' => ['create', 'edit']]);
