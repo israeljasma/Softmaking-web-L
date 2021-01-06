524529d50433239db56f7b6635621b2fe3afd720
Israel Jasma <by_darklancer@live.cl>
2021-01-06 02:38:17 -0300

    Feat: CRUD invoices api

--- app/Http/Controllers/Admin/invoicesDocumentController.php
+++ app/Http/Controllers/Admin/invoicesDocumentController.php
@@ -13,10 +13,10 @@ use Gate;
 
 class invoicesDocumentController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
     /**
      * Display a listing of the resource.
      *
@@ -24,8 +24,18 @@ class invoicesDocumentController extends Controller
      */
     public function index(User $user)
     {
-        $invoices = InvoiceDocument::where('user_id', $user->getKey())->get();
-        return view('admin.invoices.index', compact('invoices'))->with('user', $user);
+        try {
+            if(!$user){
+                return response()->json(['message' => 'Error: Users were not found.'], 412);
+            }
+
+            $invoices = InvoiceDocument::where('user_id', $user->getKey())->get();
+
+            return response()->json(['user' => $user, 'invoices' => $invoices], 200);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'message' => 'Error: invoice were not found.'], 412);
+        }
     }
 
     /**
@@ -42,63 +52,135 @@ class invoicesDocumentController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request, User $user)
     {
-        $this->validate($request, [
-            'name'          => 'required',
-            'description'   => 'required',
-            'date'          => 'required',
-            'file'          => 'required|mimes:pdf'
-        ]);
-        $data = new InvoiceDocument;
-        if($request->file('file')){
-            $file=$request->file('file');
-            $random = Str::random(40);
-            $fileName = $random.'.'.$file->getClientOriginalExtension();
-            $request->file('file')->storeAs('public',$fileName);
-            $data->file = $fileName;            
-        }
+        try{
+            if(Gate::denies('generic-administration')){
+                return response()->json([
+                    'message' => "Access denied. You don't have permission to access"], 403);
+            }
 
-        $data->name=$request->name;
-        $data->description=$request->description;
-        $data->date=$request->date;
-        $data->user_id=$user->id;
+            $validator = Validator::make($request->all(), [
+                'name'          => 'required',
+                'description'   => 'required',
+                'date'          => 'required',
+                'file'          => 'required|mimes:pdf'
+            ]);
 
-        $data->save();
+            if($validator->fails()) {
+                return response()->json($validator->errors(), 412);
+            }
 
-        $invoices = InvoiceDocument::where('user_id', $user->getKey())->get();
+            $invoice = new InvoiceDocument([
+                'name' => $request->name,
+                'description' => $request->description,
+                'date' => $request->date,
+            ]);
 
-        return redirect()->route('admin.invoices.index', [$user]);
+            if($request->file('file')){
+                $file=$request->file('file');
+                $random = Str::random(40);
+                $fileName = $random.'.'.$file->getClientOriginalExtension();
+                $request->file('file')->storeAs('public',$fileName);
+                $invoice->file = $fileName;       
+            }else{
+                return response()->json(['message' => 'Error: The invoice was not created.'], 412);
+            }
+
+            $invoice->save();
+
+            $invoices = InvoiceDocument::where('user_id', $user->getKey())->get();
+
+            return response()->json(['user' => $user, 'invoices' => $invoices], 200);
+
+            }catch(\Exception $exception){
+                return response()->json(['message' => 'Error: The invoice was not created.'], 412);
+            }
+
+
+
+        // $this->validate($request, [
+        //     'name'          => 'required',
+        //     'description'   => 'required',
+        //     'date'          => 'required',
+        //     'file'          => 'required|mimes:pdf'
+        // ]);
+        // $data = new InvoiceDocument;
+        // if($request->file('file')){
+        //     $file=$request->file('file');
+        //     $random = Str::random(40);
+        //     $fileName = $random.'.'.$file->getClientOriginalExtension();
+        //     $request->file('file')->storeAs('public',$fileName);
+        //     $data->file = $fileName;            
+        // }
+
+        // $data->name=$request->name;
+        // $data->description=$request->description;
+        // $data->date=$request->date;
+        // $data->user_id=$user->id;
+
+        // $data->save();
+
+        // $invoices = InvoiceDocument::where('user_id', $user->getKey())->get();
+
+        // return redirect()->route('admin.invoices.index', [$user]);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\User  $user
+     * @param  \App\InvoiceDocument  $invoice
      * @return \Illuminate\Http\Response
      */
     public function show(User $user, InvoiceDocument $invoice)
     {
-        $invoiceUser = InvoiceDocument::find($invoice->id);
-        return view('admin.invoices.show', compact('invoiceUser'))->with('user', $user);
-        //return view('admin.invoices.show')->with('user', $user);
+        try {
+            if(!User::findOrFail($user->id)){
+                return response()->json(['message' => 'Error: Users were not found.'], 412);
+            }
+            
+            $invoice = InvoiceDocument::where('id', $invoice->id)->where('user_id', $user->id)->get();
+            
+            if(!$invoice){
+                return response()->json(['message' => 'Error: Users were not found.'], 412);
+            }
+
+            return response()->json(['user' => $user, 'invoices' => $invoice], 200);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'message' => 'Error: The client was not found.'], 412);
+        };
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\User  $user
+     * @param  \App\InvoiceDocument  $invoice
      * @return \Illuminate\Http\Response
      */
     public function edit(User $user, InvoiceDocument $invoice)
     {
-        if(Gate::denies('edit-invoices')){
-            return redirect()->route('home');
-        }
-        $invoiceUser = InvoiceDocument::find($invoice->id);
-        return view('admin.invoices.edit', compact('invoiceUser'))->with('user', $user);
+        try {
+            if(!User::findOrFail($user->id)){
+                return response()->json(['message' => 'Error: Users were not found.'], 412);
+            }
+            
+            $invoice = InvoiceDocument::where('id', $invoice->id)->where('user_id', $user->id)->get();
+            
+            if(!$invoice){
+                return response()->json(['message' => 'Error: Users were not found.'], 412);
+            }
+
+            return response()->json(['user' => $user, 'invoices' => $invoice], 200);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'message' => 'Error: The client was not found.'], 412);
+        };
     }
 
     /**
@@ -106,47 +188,125 @@ class invoicesDocumentController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\User  $user
+     * @param  \App\InvoiceDocument  $invoice
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, User $user, InvoiceDocument $invoice)
     {
-        $invoiceUser = InvoiceDocument::find($invoice->id);
-        if($request->file){
-            $file=$request->file('file');
-            $random = Str::random(40);
-            $fileName = $random.'.'.$file->getClientOriginalExtension();
-            $request->file('file')->storeAs('public',$fileName);
-            Storage::delete('public/'.$invoiceUser->file);
-            $invoiceUser->file = $fileName;
-        }
-        $invoiceUser->name=$request->name;
-        $invoiceUser->description=$request->description;
-        $invoiceUser->date=$request->date;
-        $invoiceUser->user_id=$user->id;
+        try{
+            // if(Gate::denies('generic-administration')){
+            //     return response()->json([
+            //         'message' => "Access denied. You don't have permission to access"], 403);
+            // }
 
-        $invoiceUser->save();
+            $validator = Validator::make($request->all(), [
+                'name'          => 'required',
+                'description'   => 'required',
+                'date'          => 'required',
+                'file'          => 'required|mimes:pdf'
+            ]);
+
+            if($validator->fails()) {
+                return response()->json($validator->errors(), 412);
+            }
+            
+            $invoice = InvoiceDocument::findOrFail($invoice->id);
+
+            $invoice->fill([
+                'name' => $request->name,
+                'description' => $request->description,
+                'date' => $request->date,
+            ]);
+
+            if($request->hasfile('file')){
+                $file=$request->file('file');
+                $random = Str::random(40);
+                $fileName = $random.'.'.$file->getClientOriginalExtension();
+                $request->file('file')->storeAs('public',$fileName);
+                $invoice->file = $fileName;       
+            }else{
+                return response()->json(['message' => 'Error: The invoice was not created.'], 412);
+            }
+
+            $invoice->save();
+
+            $invoices = InvoiceDocument::where('user_id', $user->getKey())->get();
+
+            return response()->json(['user' => $user, 'invoices' => $invoices], 200);
+
+            }catch(\Exception $exception){
+                return response()->json(['message' => 'Error: The invoice was not created.'], 412);
+            }
+
+        // $invoiceUser = InvoiceDocument::find($invoice->id);
+        // if($request->file){
+        //     $file=$request->file('file');
+        //     $random = Str::random(40);
+        //     $fileName = $random.'.'.$file->getClientOriginalExtension();
+        //     $request->file('file')->storeAs('public',$fileName);
+        //     Storage::delete('public/'.$invoiceUser->file);
+        //     $invoiceUser->file = $fileName;
+        // }
+        // $invoiceUser->name=$request->name;
+        // $invoiceUser->description=$request->description;
+        // $invoiceUser->date=$request->date;
+        // $invoiceUser->user_id=$user->id;
+
+        // $invoiceUser->save();
         
-        return redirect()->route('admin.invoices.index',['user' => $user->id]);
+        // return redirect()->route('admin.invoices.index',['user' => $user->id]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\User  $user
+     * @param  \App\InvoiceDocument  $invoice
      * @return \Illuminate\Http\Response
      */
     public function destroy(User $user, InvoiceDocument $invoice)
     {
-        if(Gate::denies('delete-invoices')){
-            return redirect()->route('home');
-        }
+        try {
+            if(Gate::denies('delete-invoices')){
+                return response()->json([
+                    'message' => "Access denied. You don't have permission to access"], 403);
+            }
 
-        $invoice = InvoiceDocument::find($invoice->id);
-        Storage::delete('public/'.$invoice->file);
-        $invoice->delete();
+            if(!User::findOrFail($user->id)){
+                return response()->json(['message' => 'Error: Users were not found.'], 412);
+            }
+            
+            $invoice = InvoiceDocument::where('id', $invoice->id)->where('user_id', $user->id)->get();
+            
+            if(!$invoice){
+                return response()->json(['message' => 'Error: Users were not found.'], 412);
+            }
+
+            //$invoice = InvoiceDocument::find($invoice->id);
+            Storage::delete('public/'.$invoice->file);
+            $invoice->delete();
+
+            return response()->json([
+                'message' => 'Successfully deleted invoice!'], 201);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'message' => 'Error: The invoice was not found.'], 412);
+        };
+
+
+
+        // if(Gate::denies('delete-invoices')){
+        //     return redirect()->route('home');
+        // }
+
+        // $invoice = InvoiceDocument::find($invoice->id);
+        // Storage::delete('public/'.$invoice->file);
+        // $invoice->delete();
         
-        $invoices = InvoiceDocument::all();
-        return redirect()->route('admin.invoices.index', ['user' => $user]);
+        // $invoices = InvoiceDocument::all();
+        // return redirect()->route('admin.invoices.index', ['user' => $user]);
+        
+        
         //dd(Storage::delete('storage/app/public/'.$invoice->file));
         //dd($files = Storage::files('public'));
         
@@ -176,13 +336,13 @@ class invoicesDocumentController extends Controller
     }
 
 
-    public function downloaddd(User $user, InvoiceDocument $invoice)
-    {
-        //dd($invoice);
-        $url = Storage::temporaryUrl(
-            $invoice->file, now()->addMinutes(5)
-        );
+    // public function downloaddd(User $user, InvoiceDocument $invoice)
+    // {
+    //     //dd($invoice);
+    //     $url = Storage::temporaryUrl(
+    //         $invoice->file, now()->addMinutes(5)
+    //     );
 
-        return $url;
-    }
+    //     return $url;
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('users/{user}/invoices', 'Admin\invoicesDocumentController', ['except' => []]);
+
 //Routes without auth
 Route::resource('clients', 'Admin\ClientsController', ['except' => ['create', 'edit']]);
 
