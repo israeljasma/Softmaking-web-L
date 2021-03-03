92fdca41da5a328304e3cfa5f63f882f902a0c16
Israel Jasma <israel.jasma@gmail.com>
2021-03-03 16:42:07 -0300

    Feat: Register y create user para el admin actualizados

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -67,6 +67,7 @@ class UsersController extends Controller
             $validator = Validator::make($request->all(), [
                 'name'          => 'required|string|max:255',
                 'lastname'      => 'required|string|max:255',
+                'phone'         => 'regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
                 'email'         => 'required|string|email|max:255|unique:users',
                 'password'      => 'required|string|min:8|confirmed'
             ]);
@@ -78,6 +79,7 @@ class UsersController extends Controller
             $user = new User([
                 'name'      => $request->name,
                 'lastname'  => $request->lastname,
+                'phone'     => $request->phone,
                 'email'     => $request->email,
                 'password'  => bcrypt($request->password),
                 'active'    => True
@@ -164,6 +166,8 @@ class UsersController extends Controller
 
             $validator = Validator::make($request->all(), [
                 'name'          => 'required',
+                'lastname'      => 'required|string|max:255',
+                'phone'         => 'regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
                 'email'         => 'required|email',
                 'roles'         => 'required|numeric'
             ]);
@@ -258,6 +262,8 @@ class UsersController extends Controller
         try{
             $validator = Validator::make($request->all(), [
                 'name'          => 'required',
+                'lastname'      => 'required|string|max:255',
+                'phone'         => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
                 'email'         => 'required|string|email|max:255|unique:users'
             ]);
 
